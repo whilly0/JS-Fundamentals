a189f17086f33db24c07902c508e76ef58c2b6fd
whilly0 <josephwltd@gmail.com>
2025-05-23 13:44:02 +0100

    resolve merge conflicts

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,13 +1,10 @@
-const args = "process.argv.slice(2)";
+const args = process.argv.slice(2);
 
 if (args.length === 0) {
     console.log("No argument")
 } else if (args.length === 1) {
     console.log("Argument found")
 } else {
-<<<<<<< HEAD
-    console.log("Arguments found")
-}
-=======
     console.log("Arguments found") 
->>>>>>> 6c934dbb2813b057a00fc9c9bc63b27245eeca54
+}
+
